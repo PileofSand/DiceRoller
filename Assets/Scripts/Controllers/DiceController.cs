3aa73936c67bef72c8a9485029ffb33ccaec2b06
sir.sandman <sir.sandman@theknightsofunity.com>
2023-04-07 16:15:44 +0200

    Added movement of dice and drag and roll mechanics

--- Assets/Scripts/Controllers/DiceController.cs
+++ Assets/Scripts/Controllers/DiceController.cs
@@ -7,15 +7,18 @@ public class DiceController : MonoBehaviour
     #region VARIABLES
     [SerializeField]
     private Camera _mainCamera;
-    [SerializeField]
-    private string destinationTag = "DropArea";
-    [SerializeField, Tooltip("Speed multiplier for the dragging, used on physics object.")]
-    private float mouseDragPhysicsSpeed = 10;
-    [SerializeField, Tooltip("Speed damp for the dragging, used on a non-physics object.")]
-    private float mouseDragSpeed = .1f;
+    [SerializeField, Tooltip("Speed multiplier for releasing the dice")]
+    private float _mouseDragPhysicsSpeed = 10;
+    [SerializeField, Tooltip("Speed multiplier for the dragging, used on object.")]
+    private float _mouseDragSpeed = 0.5f;
+    [SerializeField, Tooltip("Height of dice when picking it up")]
+    private float m_pickUpHeight = 0.5f;
 
-    private Vector3 velocity = Vector3.zero;
     private Rigidbody _rigidbody;
+    private Vector3 m_velocity;
+    private Plane _plane;
+    private bool m_isDragging;
+    private Vector3 m_directionNormalized;
 
     #endregion
 
@@ -24,29 +27,39 @@ public class DiceController : MonoBehaviour
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _plane = new Plane(Vector3.up, transform.position); // define a plane that is parallel to the ground and passes through the dice object
+    }
+
+    private void FixedUpdate()
+    {
+        if (!m_isDragging)
+        {
+            return;
+        }
+
+        Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (_plane.Raycast(ray, out float initialDistance))
+        {
+            Vector3 dragPosition = ray.GetPoint(initialDistance);
+            m_directionNormalized = (dragPosition - transform.position).normalized;
+            transform.position = Vector3.SmoothDamp(transform.position, dragPosition, ref m_velocity, _mouseDragSpeed);
+        }
     }
 
     private void OnMouseUp()
     {
+        m_isDragging = false;
         _rigidbody.useGravity = true;
+        _rigidbody.velocity = m_directionNormalized * _mouseDragPhysicsSpeed;
     }
 
     private void OnMouseDown()
     {
+        m_isDragging = true;
         _rigidbody.useGravity = false;
-    }
-
-    private void OnMouseDrag()
-    {
-        float initialDistance = Vector3.Distance(transform.position, _mainCamera.transform.position);
-        Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
-        Vector3 direction = (ray.GetPoint(initialDistance) - transform.position).normalized;
-        _rigidbody.velocity = direction * mouseDragPhysicsSpeed;
-        transform.position = Vector3.SmoothDamp(transform.position, ray.GetPoint(initialDistance), ref velocity, mouseDragSpeed);
+        _rigidbody.velocity = Vector3.zero;
+        transform.Translate(Vector3.up * m_pickUpHeight);
     }
 
     #endregion
-
-
-
 }
---
+++ Assets/Scripts/Dice.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class Dice : MonoBehaviour
+{
+    public GameObject textPrefab; // Text prefab for displaying the numbers
+    public float yOffset = 0.1f; // Distance between the dice and the text
+
+    // Array of numbers to display on the dice
+    private int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+
+    void Start()
+    {
+        // Get the mesh of the dice
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+
+        // Create the text objects for each face
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            // Find the center of the face by averaging the vertices
+            int[] triangles = mesh.triangles.Where((tri, idx) => idx / 3 == i).ToArray();
+            Vector3 center = Vector3.zero;
+            for (int j = 0; j < triangles.Length; j++)
+            {
+                center += mesh.vertices[triangles[j]];
+            }
+            center /= triangles.Length;
+
+            // Calculate the position for the text object
+            Vector3 pos = transform.TransformPoint(center);
+            pos.y += yOffset;
+
+            // Create a new text object and set its position and rotation
+            GameObject textObj = Instantiate(textPrefab, pos, Quaternion.identity);
+            textObj.transform.parent = transform;
+
+            // Get the normal of the face and calculate the rotation of the text object
+            Vector3 normal = mesh.normals[triangles[0]];
+            Vector3 forward = -normal;
+            Vector3 up = Vector3.up;
+            if (normal == up || normal == -up)
+            {
+                up = Vector3.forward;
+            }
+            Quaternion rotation = Quaternion.LookRotation(forward, up);
+
+            // Set the rotation and text of the object
+            textObj.transform.rotation = rotation;
+            textObj.GetComponent<TextMeshPro>().text = numbers[i].ToString();
+        }
+    }
+}
